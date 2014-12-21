db5b584527f44f3abda004e960b0279da22e180d
Tadas <tadcka89@gmail.com>
2014-12-22 01:47:01 +0200

    Add MapperTree and MapperCollection class.

---
+++ src/Tadcka/Mapper/Type/Collection/MapperCollection.php
@@ -0,0 +1,69 @@
+<?php
+
+/*
+ * This file is part of the Tadcka package.
+ *
+ * (c) Tadas Gliaubicas <tadcka89@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Tadcka\Mapper\Type\Collection;
+
+use Tadcka\Mapper\MapperItemInterface;
+use Tadcka\Mapper\MapperSourceInterface;
+
+/**
+ * @author Tadas Gliaubicas <tadcka89@gmail.com>
+ *
+ * @since 12/22/14 1:43 AM
+ */
+class MapperCollection implements MapperSourceInterface, \Countable
+{
+    /**
+     * @var string
+     */
+    private $name;
+
+    /**
+     * @var array|MapperItemInterface[]
+     */
+    private $collection;
+
+    /**
+     * @param string $name
+     * @param array|MapperItemInterface[] $collection
+     */
+    public function __construct($name, array $collection)
+    {
+        $this->name = $name;
+        $this->collection = $collection;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Get collection
+     *
+     * @return array|MapperItemInterface[]
+     */
+    public function getCollection()
+    {
+        return $this->collection;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function count()
+    {
+        return count($this->collection);
+    }
+}
---
+++ src/Tadcka/Mapper/Type/Tree/MapperTree.php
@@ -0,0 +1,62 @@
+<?php
+
+/*
+ * This file is part of the Tadcka package.
+ *
+ * (c) Tadas Gliaubicas <tadcka89@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Tadcka\Mapper\Type\Tree;
+
+use Tadcka\Mapper\MapperSourceInterface;
+
+/**
+ * @author Tadas Gliaubicas <tadcka89@gmail.com>
+ *
+ * @since 12/22/14 1:40 AM
+ */
+class MapperTree implements MapperSourceInterface
+{
+    /**
+     * @var string
+     */
+    private $name;
+
+    /**
+     * @var MapperTreeItemInterface
+     */
+    private $tree;
+
+    /**
+     * Constructor.
+     *
+     * @param $name
+     * @param MapperTreeItemInterface $tree
+     */
+    public function __construct($name, MapperTreeItemInterface $tree)
+    {
+        $this->name = $name;
+        $this->tree = $tree;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Get tree.
+     *
+     * @return MapperTreeItemInterface
+     */
+    public function getTree()
+    {
+        return $this->tree;
+    }
+}
