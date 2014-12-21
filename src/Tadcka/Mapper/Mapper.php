2af84ea45903a39a14f43b5f6fa364f25eec1702
Tadas <tadcka89@gmail.com>
2014-12-22 01:31:43 +0200

    Add mapper class.

---
+++ src/Tadcka/Mapper/Mapper.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+ * This file is part of the Tadcka package.
+ *
+ * (c) Tadas Gliaubicas <tadcka89@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Tadcka\Mapper;
+
+/**
+ * @author Tadas Gliaubicas <tadcka89@gmail.com>
+ *
+ * @since 12/22/14 1:30 AM
+ */
+class Mapper
+{
+    const NAME = 'tadcka_mapper';
+}
